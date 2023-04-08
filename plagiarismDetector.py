533d57b5470730de504d65be7dc751073fb07f8a
Lance Rogan <lance.rogan167@gmail.com>
2023-04-07 17:28:38 -0700

    Minor initialization refactoring

--- plagiarismDetector.py
+++ plagiarismDetector.py
@@ -10,29 +10,33 @@ dataFolderPath = ""
 allDataFileName = 'masterData.csv'
 sampleDataFileName = 'sampleData.csv'
 
+
+
 def plotResults():
-    print("print results")
+    print("Printing results")
 
 def createSampleCSV():
     global allDataDF
     global sampleDataDF
-    print("creating sample csv")
+    global sampleDataFileName
+    print("Creating "+ sampleDataFileName +" now.")
     sampleDataDF = allDataDF.sample(frac=0.2)
     sampleDataDF.to_csv(os.path.join(dataFolderPath,sampleDataFileName), index=False)
 
 
 def checkForPlagiarism():
-    print("going to check now")
+    print("Checking for Plagiarism.")
+    
 
 
 def createCombinedCSV():
     global allDataDF
+    global allDataFileName
+
+    print("Creating "+ allDataFileName +" now.")
     for currentFile in os.listdir(dataFolderPath):
             if currentFile.endswith('.csv'):
-
-
                 currentFilePath = os.path.join(dataFolderPath,currentFile)
-
                 try:
                     tempDF = pd.read_csv(currentFilePath,usecols=[0,1,3],encoding='utf-8')
                     allDataDF = pd.concat([allDataDF,tempDF],ignore_index=True)
@@ -40,8 +44,9 @@ def createCombinedCSV():
                     print(currentFile + " has some empty values or columns. Skipping file.")
     allDataDF.to_csv(os.path.join(dataFolderPath,allDataFileName), index=False)
 
+def plagiarismDetectorInitialization():
 
-def runPlagiarismDetector():
+    print("Starting Plagiarism Detector initialization.")
     global dataFolderPath
     global allDataFileName
     global sampleDataFileName
@@ -49,32 +54,37 @@ def runPlagiarismDetector():
     global allDataDF
     global sampleDataDF
 
-    # Get the current operating system
     if os.name == 'nt':  # Windows
         dataFolderPath = '.\\data'   # Use backslash for Windows
     else:  # Mac or Linux
         dataFolderPath = './data'   # Use forward slash for Mac or Linux
 
-    print(os.path.join(dataFolderPath,allDataFileName))
-
     #Create the combined csv file if it does not exist
     if (not (os.path.exists(os.path.join(dataFolderPath,allDataFileName)))):
+        print("Combined CSV file "+ allDataFileName + " does not exist.")
         createCombinedCSV()
     else:
+        print(allDataFileName + " exists.")
         print("Reading "+allDataFileName+" into data frame.")
         allDataDF = pd.read_csv(os.path.join(dataFolderPath,allDataFileName),encoding='utf-8')
 
     if (not (os.path.exists(os.path.join(dataFolderPath,sampleDataFileName)))):
+        print("Sample CSV file "+ sampleDataFileName + " does not exist.")
         createSampleCSV()
     else:
+        print(sampleDataFileName + " exists.")
         print("Reading " + sampleDataFileName + " into data frame.")
         sampleDataDF = pd.read_csv(os.path.join(dataFolderPath,sampleDataFileName),encoding='utf-8')
     
-    
+    print("Plagiarism Detector initialization complete.")
+
+
+def runPlagiarismDetector():
+    print("Starting Plagiarism Detector.")
+    plagiarismDetectorInitialization()
     checkForPlagiarism()
     plotResults()
 
 
 #Run Detector
 runPlagiarismDetector()
-print(allDataDF)
